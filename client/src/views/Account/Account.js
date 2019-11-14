2df33e1779e6e15666ecc68ff475ce58a732751a
MariaWan <43481803+MariaWan@users.noreply.github.com>
2019-11-14 17:47:55 -0500

    styling fix

--- client/src/views/Account/Account.js
+++ client/src/views/Account/Account.js
@@ -10,22 +10,22 @@ class Account extends Component{
 		return(
 		<div className = "UserAccount">
 			<div className = "ProfilePic">
-			<div className = "container-fluid d-flex justify-content-center">
-				<div className = "card text-center shadow"> 
+			<div className = "container-fluid d-flex justify-content-center" id="containerfluid">
+				<div className = "card text-center shadow" id="dishcard"> 
 					<div className = "overflow">
 						<img src ={ "./tempChefProPic.png" } alt = "tempDish1" className="card-img-top"/>
 					</div>
 			
-						<h4 className = "card-title"> Gordon Ramsay</h4>
+						<h4 className = "card-title" id="cardtitle"> Gordon Ramsay</h4>
 					
 				</div>
 			</div>
 			</div>
 		<div className="DishCards">
-			<div className = "container-fluid d-flex justify-content-center">
+			<div className = "container-fluid d-flex justify-content-center" id="containerfluid">
 				<div className = "row">
 					<div className="col-md-4">
-						<Card imgsrc = {"/tempDish1.png"} title="Chicken Curry"/>
+						<Card imgsrc = {"/tempDish1.png"} title="Chicken Curry" description = "curry chicken"/>
 					</div>
 					<div className="col-md-4">
 						<Card imgsrc = {"/tempDish2.png"} title="Falafel"/>
