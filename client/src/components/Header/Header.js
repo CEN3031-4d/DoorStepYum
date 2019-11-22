e9983332af095398ffaf7e61483693cb8c3e6a7b
Natalia Lisiecka <nlisiecka@ufl.edu>
2019-11-22 13:32:55 -0500

    something

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,20 +6,20 @@ const Header = () => {
     return (
 
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a id="logopic" class="nav-link" href="/Home">
+			<img id="logopic" class="img-fluid" src={ "/yumlogo.png" }/> 
+			</a>
+
 			<div class="container-fluid">
-				<a class="navbar-brand" href="#"></a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
 		<div class="collapse navbar-collapse" id="navbarNav">
-				
-		<a class="nav-link" href="/Home">
-			<img class="img-fluid" src={ "/logoYum.png" }/> 
-		</a>
-		<a class="nav-link" href="/About">About</a>
-		  <ul class="navbar-nav ml-auto"> 
-			<li class="nav-item "> <a class="nav-link" href="/LogIn" >Log In</a> </li>
-			<li class="nav-item "> <a class="nav-link" href="#">Register</a> </li>
-
-		  </ul>
+			<a id="tabs" class="nav-link" href="/About">About</a>
+			<a id="tabs" class="nav-link" href="/BrowsebyChef">Chefs</a>
+			<a id="tabs" class="nav-link" href="/BrowsebyDish">Dishes</a>
+			<ul class="navbar-nav ml-auto"> 
+				<li class="nav-item "> <a class="nav-link" href="/LogIn" >Log In</a> </li>
+				<li class="nav-item "> <a class="nav-link" href="#">Register</a> </li>
+		  	</ul>
 		</div>
 	  </div>
 	</nav>
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -29,7 +29,7 @@ function Home() {
 		<div class="row">
 		  <div class="col-12 text-center">
 			<div class="h1">What We Do</div>
-			<div class="section-body">Doorstep Yummy aims to bring the pleasures of a Michelin starred restaraunt straight to your door. Our diverse team of world-class chefs are trained in preparing the finest, most authentic global cuisine. Scroll down to find out more about our chefs and their specialties.
+			<div class="section-body">Yummy! aims to bring the pleasures of a Michelin starred restaraunt straight to your door. Our diverse team of world-class chefs are trained in preparing the finest, most authentic global cuisine. Scroll down to find out more about our chefs and their specialties.
 			</div>
 			<a href="#" class="btn btn-outline-primary">
 					<Link to="/BrowsebyChef">Our Chefs</Link>
@@ -73,7 +73,7 @@ function Home() {
 	</section>
 	<footer id="page-footer" className="bg-light">
       <div className="container">
-        <span className="text-muted">Doorstep Yummy © 2019</span>
+        <span className="text-muted">Yummy © 2019</span>
 		</div>
     </footer>
 	</div>
