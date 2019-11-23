5381eb89ef9828c711567d5f181c5b2668b61426
DavidSR56 <davidasingh@ufl.edu>
2019-11-23 16:58:16 -0500

    Header.js fixes

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,7 +4,6 @@ import './Header.css';
 
 const Header = () => {
     return (
-<<<<<<< HEAD
 
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container-fluid">
@@ -20,19 +19,6 @@ const Header = () => {
 			<li class="nav-item "> <a class="nav-link" href="/LogIn" >Chef</a> </li>
 			<li class="nav-item "> <a class="nav-link" href="#">Customer</a> </li>
 
-=======
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<div className="container-fluid">
-				<a className="navbar-brand" href="#"></a>
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span> </button>
-		<div className="collapse navbar-collapse" id="navbarNav">
-		<a className="nav-link" href="http://localhost:3000/Home#">
-			<img className="img-fluid" src={ "/logoYum.png" }/> 
-		</a>
-		  <ul className="navbar-nav ml-auto"> 
-			<li className="nav-item "> <a className="nav-link" href="http://localhost:3000/LogIn#" >Log In</a> </li>
-			<li className="nav-item "> <a className="nav-link" href="#">Register</a> </li>
->>>>>>> Maria
 		  </ul>
 		</div>
 	  </div>
