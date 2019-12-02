22a334e6d4d5c38876fb00a2d70c81dcdac42635
Vivian Garcia <garcviv@ufl.edu>
2019-12-02 18:28:36 -0500

    format

--- client/src/views/BrowsebyDish/CardUI.js
+++ client/src/views/BrowsebyDish/CardUI.js
@@ -11,7 +11,7 @@ return(
 				<p className ="card-text text-secondary" id="cardtext">
 				{props.description}
 				</p>
-				<a href="/FoodProfile/" className ="btn btn-outline-success">Find out more</a>
+				<a href={"/FoodProfile/" + props.id} className ="btn btn-outline-success">Find out more</a>
 			</div>
 			</div>
 		);
