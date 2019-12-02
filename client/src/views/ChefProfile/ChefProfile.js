61fd92d10fc0b47501fdcd6765285157b20bc1e5
bVelliquette <BenjaminVelliquette@gmail.com>
2019-12-02 17:52:10 -0500

    starting Reservation Page

--- client/src/views/ChefProfile/ChefProfile.js
+++ client/src/views/ChefProfile/ChefProfile.js
@@ -12,13 +12,13 @@ import ReactDOM from 'react-dom';
 
 
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
   }
 };
 
@@ -35,32 +35,31 @@ class ChefProfile extends Component {
       chefPrice: '',
       chefPicture: '',
       image: '',
-	  time:'10:00',
-	  date:new Date(),
-	  modalIsOpen: false
+      time: '',
+      date: '',
+      modalIsOpen: false
     };
-	this.openModal = this.openModal.bind(this);
+    this.openModal = this.openModal.bind(this);
     this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
-  
+
   openModal() {
-    this.setState({modalIsOpen: true});
+    this.setState({ modalIsOpen: true });
   }
- 
+
   afterOpenModal() {
     // references are now sync'd and can be accessed.
     this.subtitle.style.color = 'gray';
-
   }
- 
+
   closeModal() {
-    this.setState({modalIsOpen: false});
+    this.setState({ modalIsOpen: false });
   }
 
- onChange = time => this.setState({ time })
- onChange = date => this.setState({ date })
-  
+  onChange = ctime => this.setState({ time: ctime })
+  onChange = date => this.setState({ date })
+
   componentDidMount() {
     axios.get('http://localhost:5000/api/chef/find/' + this.props.match.params.id)
       .then(res => {
@@ -86,100 +85,94 @@ class ChefProfile extends Component {
   render() {
     return (
       <body is="dmx-app" id="index">
-       
-          <div class="row">
-            <div class="col-12 col-md-4">
-              <div class="card" id="chefprofilecard">
-                <img id="chefprofilepic" class="card-img-top rounded-bottom ml-0" alt="Card image cap" src={this.state.image} />
-                <div class="card-body">
-                  <h4 id="chefName" class="card-title">{this.state.chefName}&nbsp;
+
+        <div class="row">
+          <div class="col-12 col-md-4">
+            <div class="card" id="chefprofilecard">
+              <img id="chefprofilepic" class="card-img-top rounded-bottom ml-0" alt="Card image cap" src={this.state.image} />
+              <div class="card-body">
+                <h4 id="chefName" class="card-title">{this.state.chefName}&nbsp;
                   </h4>
-                  <p id="bio" class="card-text">{this.state.chefBio}</p>
-                  <div class="card">
-                    <div class="card-header" id="accordion1_headingOne">
-                      <h5 class="mb-0">
-                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#accordion1_collapseOne" aria-expanded="true" aria-controls="collapseOne">Cusine Type</button>
-                      </h5>
-                    </div>
-                    <div id="accordion1_collapseOne" class="collapse" is="dmx-bs4-collapse" show="true" aria-labelledby="accordion1_headingOne" data-parent="#accordion1">
-                      <div class="card-body">
-                        Specialization text
+                <p id="bio" class="card-text">{this.state.chefBio}</p>
+                <div class="card">
+                  <div class="card-header" id="accordion1_headingOne">
+                    <h5 class="mb-0">
+                      <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#accordion1_collapseOne" aria-expanded="true" aria-controls="collapseOne">Cusine Type</button>
+                    </h5>
                   </div>
-                    </div>
+                  <div id="accordion1_collapseOne" class="collapse" is="dmx-bs4-collapse" show="true" aria-labelledby="accordion1_headingOne" data-parent="#accordion1">
+                    <div class="card-body">
+                      Specialization text
                   </div>
-
-
-                  <div class="card">
-                    <div class="card-header" id="accordion1_headingTwo">
-                      <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#accordion1_collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                          Past Experience</button>
-                      </h5>
-                    </div>
-                    <div id="accordion1_collapseTwo" class="collapse" is="dmx-bs4-collapse" aria-labelledby="accordion1_headingTwo" data-parent="#accordion1">
-                      <div class="card-body">
-                        Past experience text
                   </div>
-                    </div>
-                  </div>   
-				<button onClick={this.openModal} class="btn btn-block btn-outline-success font-weight-normal">Request this Chef</button>
-					
-					<Modal
-					  isOpen={this.state.modalIsOpen}
-					  onAfterOpen={this.afterOpenModal}
-					  onRequestClose={this.closeModal}
-						  ariaHideApp={false}
-					  style={customStyles}
-					  contentLabel="Example Modal"
-					>
-				
-					<div className = "App__Form" id="App__Form3">
-						<div className = "FormCenter">
-					
-							  <h2 className="headerRequest" ref={subtitle => this.subtitle = subtitle}>Request {this.state.chefName} </h2>
-						
-							 <div className= "selectRequest">Pick a time to reserve {this.state.chefName}</div>
-							  
-									  <form className = "FormFields">
-										 <div>
-											<Calendar
-											  onChange={this.onChange}
-											  value={this.state.date}
-											/>
-										 </div>
-										  <div className="TimePicker">
-											<TimePicker
-											  onChange={this.onChange}
-											  value={this.state.time}
-											/>
-										  </div>
-										  <div className="FormField">
-											<label id="Msg" className="FormField__Label" htmlFor="email"> Write a Message </label>
-											<textarea type="Msg" id="Msg" className ="FormField__Input" 
-											placeholder="What would you like the chef to know?" name="customerMessage" 
-											/>
-										</div>			
-									  </form>
-									  
-										<button class="btn btn-block btn-outline-success font-weight-normal">Submit Request</button>
-										<button onClick={this.closeModal} class="btn btn-block btn-outline-success font-weight-normal">close</button>
-					
-					</div>
-			</div>
-					</Modal>
-						
                 </div>
+
+
+                <div class="card">
+                  <div class="card-header" id="accordion1_headingTwo">
+                    <h5 class="mb-0">
+                      <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#accordion1_collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Past Experience</button>
+                    </h5>
+                  </div>
+                  <div id="accordion1_collapseTwo" class="collapse" is="dmx-bs4-collapse" aria-labelledby="accordion1_headingTwo" data-parent="#accordion1">
+                    <div class="card-body">
+                      Past experience text
+                  </div>
+                  </div>
+                </div>
+                <button onClick={this.openModal} class="btn btn-block btn-outline-success font-weight-normal">Request this Chef</button>
+
+                <Modal
+                  isOpen={this.state.modalIsOpen}
+                  onAfterOpen={this.afterOpenModal}
+                  onRequestClose={this.closeModal}
+                  ariaHideApp={false}
+                  style={customStyles}
+                  contentLabel="Example Modal"
+                >
+
+                  <div className="App__Form" id="App__Form3">
+                    <div className="FormCenter">
+
+                      <h2 className="headerRequest" ref={subtitle => this.subtitle = subtitle}>Request {this.state.chefName} </h2>
+
+                      <div className="selectRequest">Pick a time to reserve {this.state.chefName}</div>
+
+                      <form className="FormFields">
+                        <div>
+                          <Calendar
+                            onChange={this.onChange}
+                            value={this.state.date}
+                          />
+                        </div>
+                        <div className="TimePicker">
+                          <TimePicker
+                            onChange={this.onChange}
+                            value={this.state.time}
+                          />
+                        </div>
+                        <div className="FormField">
+                          <label id="Msg" className="FormField__Label" htmlFor="email"> Write a Message </label>
+                          <textarea type="Msg" id="Msg" className="FormField__Input"
+                            placeholder="What would you like the chef to know?" name="customerMessage"
+                          />
+                        </div>
+                      </form>
+
+                      <button class="btn btn-block btn-outline-success font-weight-normal">Submit Request</button>
+                      <button onClick={this.closeModal} class="btn btn-block btn-outline-success font-weight-normal">close</button>
+
+                    </div>
+                  </div>
+                </Modal>
+
               </div>
             </div>
-            <div class="col-12 col-md-8 align-self-start">
-             
-            
-                  
-                 
-                </div>
-              </div>
-              
-       
+          </div>
+          <div class="col-12 col-md-8 align-self-start">
+          </div>
+        </div>
       </body>
     );
   }
