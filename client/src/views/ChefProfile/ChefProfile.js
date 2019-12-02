dbce8b4c99b48ec94cb3ba089c8dcf598c3d4dcd
MariaWan <43481803+MariaWan@users.noreply.github.com>
2019-12-02 13:52:18 -0500

    PopUp

--- client/src/views/ChefProfile/ChefProfile.js
+++ client/src/views/ChefProfile/ChefProfile.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Popup from "reactjs-popup";
 import Encoder from '../../components/Encoder/Encoder'
 import './ChefProfile.css';
+import { Button } from 'react-bootstrap';
+import Modal from 'react-modal';
+import ModalPop from './ModalPop';
+import TimePicker from 'react-time-picker';
+import Calendar from 'react-calendar';
+import ReactDOM from 'react-dom';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+
 
 class ChefProfile extends Component {
   constructor(props) {
@@ -14,9 +34,32 @@ class ChefProfile extends Component {
       chefPassword: '',
       chefPrice: '',
       chefPicture: '',
-      image: ''
-    }
+      image: '',
+	  time:'10:00',
+	  date:new Date(),
+	  modalIsOpen: false
+    };
+	this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
+	
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+ 
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+ 
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+ onChange = time => this.setState({ time })
+  onChange = date => this.setState({ date })
+  
   componentDidMount() {
     axios.get('http://localhost:5000/api/chef/find/' + this.props.match.params.id)
       .then(res => {
@@ -41,6 +84,7 @@ class ChefProfile extends Component {
   }
   render() {
     return (
+	
       <body is="dmx-app" id="index">
         <div class="container wappler-block">
           <div class="modal fade" id="bookChefModal" is="dmx-bs4-modal" tabindex="-1" role="dialog">
@@ -48,9 +92,6 @@ class ChefProfile extends Component {
               <div class="modal-content">
                 <div class="modal-header">
                   <h3 class="modal-title">Book this Chef</h3>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                  </button>
                 </div>
                 <div class="modal-body"></div>
                 <h3 class="font-weight-light">Question 1</h3>
@@ -81,93 +122,45 @@ class ChefProfile extends Component {
                 <a href="#" class="badge badge-success">Pro</a>
                   </h4>
                   <p class="card-text">{this.state.chefBio}</p>
-                  <a href="#" class="btn btn-block btn-outline-success font-weight-normal" data-toggle="modal" data-target="#bookChefModal">Book this Chef</a>
+                 
+				<button onClick={this.openModal}>Open Modal</button>
+					<Modal
+					  isOpen={this.state.modalIsOpen}
+					  onAfterOpen={this.afterOpenModal}
+					  onRequestClose={this.closeModal}
+						  ariaHideApp={false}
+					  style={customStyles}
+					  contentLabel="Example Modal"
+					>
+			 
+					  <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+					  <button onClick={this.closeModal}>close</button>
+					  <div>I am a modal</div>
+							  <form>
+								<input />
+								 <div>
+									<Calendar
+									  onChange={this.onChange}
+									  value={this.state.date}
+									/>
+								 </div>
+								  <div>
+									<TimePicker
+									  onChange={this.onChange}
+									  value={this.state.time}
+									/>
+								  </div>
+								<button>tab navigation</button>
+								<button>stays</button>
+								<button>inside</button>
+								<button>the modal</button>
+							  </form>
+					</Modal>
                 </div>
               </div>
             </div>
             <div class="col-12 col-md-6 align-self-start">
-              <h1 class="text-center font-weight-normal">Specialty Dishes
-          </h1>
-              <div class="row">
-                <div class="col"><div id="specialityDishesCarousel" class="carousel slide" data-ride="carousel">
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <img class="d-block w-100" src="assets/images/slideshow/cornishcod.jpg" alt="slide #1" />
-                      <div class="carousel-caption d-none d-md-block w-auto">
-                        <h5>Cornish Cod</h5>
-                        <p>Classic English cod cooked in breadcrumbs</p>
-                      </div>
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" src="assets/images/slideshow/foiegras.jpg" alt="slide #2" />
-                      <div class="carousel-caption d-none d-md-block">
-                        <h5>Foie Gras</h5>
-                        <p>Duck liver served with peppercorn mustard sauce</p>
-                      </div>
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" src="assets/images/slideshow/wellington2.jpg" alt="slide #3" />
-                      <div class="carousel-caption d-none d-md-block">
-                        <h5>Beef Wellington</h5>
-                        <p>Gordon Ramsay's signature Beef Wellington</p>
-                      </div>
-                    </div>
-                  </div>
-                  <a class="carousel-control-prev" href="#specialityDishesCarousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                  <a class="carousel-control-next" href="#specialityDishesCarousel" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                  </a>
-                </div>
-                </div>
-              </div>
-              <div class="row"><div class="col">
-                <div class="accordion" id="accordion1">
-                  <div class="card">
-                    <div class="card-header" id="accordion1_headingOne">
-                      <h5 class="mb-0">
-                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#accordion1_collapseOne" aria-expanded="true" aria-controls="collapseOne">Specialization</button>
-                      </h5>
-                    </div>
-                    <div id="accordion1_collapseOne" class="collapse" is="dmx-bs4-collapse" show="true" aria-labelledby="accordion1_headingOne" data-parent="#accordion1">
-                      <div class="card-body">
-                        Specialization text
-                  </div>
-                    </div>
-                  </div>
-                  <div class="card">
-                    <div class="card-header" id="accordion1_headingTwo">
-                      <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#accordion1_collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                          Past Experience</button>
-                      </h5>
-                    </div>
-                    <div id="accordion1_collapseTwo" class="collapse" is="dmx-bs4-collapse" aria-labelledby="accordion1_headingTwo" data-parent="#accordion1">
-                      <div class="card-body">
-                        Past experience text
-                  </div>
-                    </div>
-                  </div>
-                  <div class="card">
-                    <div class="card-header" id="accordion1_headingThree">
-                      <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#accordion1_collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                          Reviews</button>
-                      </h5>
-                    </div>
-                    <div id="accordion1_collapseThree" class="collapse" is="dmx-bs4-collapse" aria-labelledby="accordion1_headingThree" data-parent="#accordion1">
-                      <div class="card-body">
-                        Reviews
-                  </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              </div>
-            </div>
+          </div>
           </div>
           <div class="row">
             <div class="col">
@@ -211,5 +204,4 @@ class ChefProfile extends Component {
     );
   }
 }
-
 export default ChefProfile;
